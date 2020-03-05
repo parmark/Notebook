16d22310f5fbc84bedd36d4deb7343a9fcaf7312
Kevin Parmar <parmarkevinb@gmail.com>
2020-03-04 19:59:16 -0500

    D:/Program Files/Git/api/notes now reads db.json and returns saved notes

--- server.js
+++ server.js
@@ -17,4 +17,6 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.ht
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public", "notes.html")));
 
+app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db", "db.json")))
+
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`))
