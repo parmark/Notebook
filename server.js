2409ff297a46b3a39d9679debc973beac1fd322d
Kevin Parmar <parmarkevinb@gmail.com>
2020-03-04 19:52:24 -0500

    serves notes.html on /notes

--- server.js
+++ server.js
@@ -15,5 +15,6 @@ app.use(express.static(`public`));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public", "notes.html")));
 
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`))
