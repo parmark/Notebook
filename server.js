f6277b942341d10733cfb1d2c77dc9035d4bc0d4
Kevin Parmar <parmarkevinb@gmail.com>
2020-03-05 23:06:54 -0500

    post and delete now live update page

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ var fs = require("fs")
 var app = express();
 var PORT = process.env.PORT || 2048;
 
-var notes
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,52 +18,36 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.ht
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public", "notes.html")));
 
-app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db", "db.json")))
+app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db", "db.json")));
 
 app.post("/api/notes", function(req, res) {
-
     fs.readFile(path.join(__dirname, "db", "db.json"), function(err, data) {
-        
-        var json = JSON.parse(data);
-
+        var json = JSON.parse(data)
         req.body["id"] = Math.floor(Math.random() * 100000000);
-
         json.push(req.body);
-
         fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(json), function(err) {
-            
-            if (err) {
-                console.log(err)
-            }
-
-            console.log("NEW NOTE LOGGED")
+            if (err) console.log(err)
         })
     })
 
+    res.json(req.body);
 })
 
 app.delete("/api/notes/:id", function(req, res) {
-    
     fs.readFile(path.join(__dirname, "db", "db.json"), function(err, data) {
-        
         var json = JSON.parse(data);
-
         for(i = 0; i < json.length; i++) {
             if (json[i].id === parseInt(req.params.id)) {
                 json.splice(i, 1)
-
                 fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(json), function(err) {
-            
-                    if (err) {
-                        console.log(err)
-                    }
-        
-                    console.log("NEW NOTE REMOVED")
+                    if (err) console.log(err)
                 })
             }
         }
     })
     
+
+    res.json(req.body)
 })
 
-app.listen(PORT, () => console.log(`Server is listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
